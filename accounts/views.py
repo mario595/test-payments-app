f847b3896b2ee363bf4f12e4165590f9b73cdbb2
mario595 <mariomartinez595@gmail.com>
2016-02-05 19:47:44 +0000

    bootstrap accordion config

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,13 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 
+from accounts.models import Account
+
 
 def transactions(request):
-    return HttpResponse(render(request, 'accounts/transactions.html', {}))
+    accounts = Account.objects.all()
+    context = {'accounts':accounts}
+    return HttpResponse(render(request, 'accounts/transactions.html', context))
 
 def payment(request):
     return HttpResponse(render(request, 'accounts/payment.html', {}))
