b551aee778474f7cafe7ca95bb91b4594554115e
mario595 <mariomartinez595@gmail.com>
2016-02-06 11:27:26 +0000

    refactor in views module

--- accounts/views.py
+++ accounts/views.py
@@ -25,13 +25,14 @@ def payment(request):
         if form.is_valid():
             # process the data in form.cleaned_data as required
             try:
+                #Retrieve transaction from form
                 transaction = form.save(commit=False)
+                #create factory
                 factory = CommandFactory()
-                cmd = factory.create_command("TRANS", transaction)
-                cmd.execute()
-                messages.success(request, "Transfer succesful")
-                cmd = factory.create_command("NOTIFY", transaction)
-                cmd.execute()
+                #Try the transaction, if something goes wrong, an exception will be raised
+                _make_transaction(request, transaction, factory)
+                #Notify the transaction
+                _notify_transaction(transaction, factory)
                 
             except CommandError as ce:
                 messages.error(request, ("Transfer unsuccesful: %s" % ce))
@@ -47,3 +48,13 @@ def payment(request):
 
     return render(request, 'accounts/payment.html', {'form': form})
 #     return HttpResponse(render(request, 'accounts/payment.html', {}))
+
+def _make_transaction(request, transaction, factory):
+    cmd = factory.create_command("TRANS", transaction)
+    cmd.execute()
+    messages.success(request, "Transfer succesful")
+
+
+def _notify_transaction(transaction, factory):
+    cmd = factory.create_command("NOTIFY", transaction)
+    cmd.execute()
